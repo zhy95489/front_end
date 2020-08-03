adcaa81f88c4775c24fb6e96d664ab6bce0e8ced
zhy <954891090@qq.com>
2020-08-03 15:49:25 +0800

    添加了点击图片出寄语的功能

--- lechisoftIndex/js/随机分布图片.js
+++ lechisoftIndex/js/随机分布图片.js
@@ -1,3 +1,7 @@
+$(function () { 
+	$("[data-toggle='popover']").popover();
+});
+
 
 for (var i = 1; i < 20; i++) {
     var StringTitles = ["富强民主文明和谐","爱国敬业诚信友善","会当凌绝顶，一览众山小","这一辈子，你哟没有为别人拼过命","这一路你可以哭，但不能怂"]
@@ -9,8 +13,7 @@ for (var i = 1; i < 20; i++) {
     var figcaption = document.createElement("figcaption");
     //创建说话的文体
     var imgText = document.createElement("p");
-    var textnode = document.createTextNode(i+"这是祝福的话,")
-    imgText.appendChild(textnode);
+
     
     //添加src属性
     imgObj.setAttribute("src", "./images/" + i + ".jpg");
@@ -30,7 +33,6 @@ for (var i = 1; i < 20; i++) {
     //添加width属性 getRandom()随机数函数
     // var width = rand(100, 300);
    
-    console.log(x,y,width);
     imgObj.setAttribute("width", width);
     //设置坐标 x y 为未知数
     var styles = "position:fixed;left:" + x + "px;top:" + y + "px;-webkit-transform: rotate(" + xed + "deg);-moz-transform: rotate(" + yed + "deg);"
@@ -41,7 +43,16 @@ for (var i = 1; i < 20; i++) {
     //添加title文字
     var indexs = rand(0, 4);
     var StringTitle = StringTitles[indexs];
-    imgObj.setAttribute('title',StringTitle)
+    imgObj.setAttribute('title','这是来自这位作者的寄语。。。')
+    imgObj.setAttribute('data-toggle',"popover");
+    if(window.innerWidth - x < 300){
+        imgObj.setAttribute('data-placement','left');
+    }
+    imgObj.setAttribute('data-content',StringTitle);
+
+
+
+
     //添加onclick事件属性
     //this 代表当前对象,this是一个对象,只能在函数内使用
     imgObj.setAttribute("onclick", "removeImg(this)");
@@ -64,3 +75,4 @@ function onmouseover(over){
     over.setAttribute("style","opacity:1;")
 }
 
+ 
