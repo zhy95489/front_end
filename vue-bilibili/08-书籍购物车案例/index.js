46bfa15541cae3c49f90971f8eb52bce583a3acd
zhy95489 <964891090@qq.com>
2020-08-27 21:05:13 +0800

    提交了案例购物车案例

---
+++ vue-bilibili/08-书籍购物车案例/index.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+    el:"#app",
+    data: {
+        books: [{name:'算法导论',date:'2006-9',prace:'85',num:0},
+                {name:'java',date:'2012-12',prace:'75',num:0},
+                {name:'oracle',date:'2032-7',prace:'90',num:0}]
+    }
+})
