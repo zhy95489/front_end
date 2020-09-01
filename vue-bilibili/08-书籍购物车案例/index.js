d70a60c9068c67cf743f5e8931b930f3f1f559db
zhy95489 <964891090@qq.com>
2020-09-01 23:07:55 +0800

    复习题  购物车小案例

--- vue-bilibili/08-书籍购物车案例/index.js
+++ vue-bilibili/08-书籍购物车案例/index.js
@@ -1,8 +1,38 @@
 const app = new Vue({
-    el:"#app",
+    el: "#app",
     data: {
-        books: [{name:'算法导论',date:'2006-9',prace:'85',num:0},
-                {name:'java',date:'2012-12',prace:'75',num:0},
-                {name:'oracle',date:'2032-7',prace:'90',num:0}]
+        books: [{id: 1, name: '算法导论', date: '2006-9', price: 85.00, num: 1},
+            {id: 2, name: 'java', date: '2012-12', price: 75.00, num: 1},
+            {id: 3, name: 'oracle', date: '2032-7', price: 90.00, num: 1}],
+    },
+    methods: {
+        // getFinalPrice: function (price) {
+        //
+        // },
+
+        numSub: function (index) {
+            this.books[index].num --
+        },
+        numAdd: function (index) {
+            this.books[index].num ++
+
+        },
+        remove:function (index) {
+            this.books.splice(index,1);
+        }
+    },
+    filters:{
+        getPrice(price){
+            return '¥' + price.toFixed(2);
+        }
+    },
+    computed:{
+        totalPrice:function () {
+            let totalPrice = 0;
+            for (let i = 0; i < this.books.length; i++) {
+                totalPrice += this.books[i].price*this.books[i].num
+            }
+            return totalPrice;
+        }
     }
 })
